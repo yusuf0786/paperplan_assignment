e6a15afaacd61651376179e6c6137d142c30f670
yusuf0786 <yusssuf0786@gmail.com>
2022-02-23 16:39:00 +0530

    another commit

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ $( function(){
         $(".mynav_right").slideToggle()
     })
 
+    let items = document.querySelectorAll(".items")
+    
+    for(let i = 0; i <= items.length-1; i++){
+        $($(".items")[i]).mouseover( function(){
+            $($(".other_btns")[i]).addClass("other_btns_anim")
+        })
+        $($(".items")[i]).mouseout( function(){
+            $($(".other_btns")[i]).removeClass("other_btns_anim")
+        })
+    }
+
 });
